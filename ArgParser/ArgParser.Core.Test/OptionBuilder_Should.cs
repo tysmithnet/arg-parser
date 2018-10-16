21ab51c2220075e571a76e725edc5bd796b66bb1
Ty Smith <dts5004@gmail.com>
2018-10-15 22:17:10 -0700

    Initial commit

---
+++ ArgParser/ArgParser.Core.Test/OptionBuilder_Should.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Linq;
+using FluentAssertions;
+using Xunit;
+
+namespace ArgParser.Core.Test
+{
+    public class OptionBuilder_Should
+    {
+        private class WhateverOptions : IOptions
+        {
+            public string[] OtherGuys { get; set; }
+            public int[] OtherThings { get; set; }
+            public bool Quiet { get; set; }
+            public string Source { get; set; }
+            public string Target { get; set; }
+            public string[] Things { get; set; }
+            public bool Verbose { get; set; }
+        }
+
+        [Fact]
+        public void Pass_Basic_Test_Cases()
+        {
+            // arrange
+            var options = new WhateverOptions();
+            var options2 = new WhateverOptions();
+            var builder = new OptionsBuilder<WhateverOptions>()
+                .WithBoolean('v', whateverOptions => whateverOptions.Verbose = true)
+                .WithMultipleSwitch('t', (whateverOptions, strings) => whateverOptions.Things = strings)
+                .WithMultipleSwitch('o',
+                    (whateverOptions, strings) =>
+                        whateverOptions.OtherThings = strings.Select(x => Convert.ToInt32(x)).ToArray())
+                .WithPositional(1, (whateverOptions, strings) => whateverOptions.Source = strings[0])
+                .WithPositional(-1, (whateverOptions, strings) => whateverOptions.OtherGuys = strings);
+
+            // act
+            builder.Parse(options, "-v source otherguy1 otherguy2 -t thing1 thing2 -o 1 2 3".Split(' '));
+            builder.Parse(options2, "source otherguy1 otherguy2 -o 1 2 3 -t thing1 thing2 -v".Split(' '));
+
+            // assert
+            options.Verbose.Should().BeTrue();
+            options.Source.Should().Be("source");
+            options.OtherGuys.Should().BeEquivalentTo("otherguy1", "otherguy2");
+            options.Things.Should().BeEquivalentTo("thing1", "thing2");
+            options.OtherThings.Should().BeEquivalentTo(new[] {1, 2, 3});
+
+            options2.Verbose.Should().BeTrue();
+            options2.Source.Should().Be("source");
+            options2.OtherGuys.Should().BeEquivalentTo("otherguy1", "otherguy2");
+            options2.Things.Should().BeEquivalentTo("thing1", "thing2");
+            options2.OtherThings.Should().BeEquivalentTo(new[] { 1, 2, 3 });
+        }
+
+        [Fact]
+        public void Return_Only_As_Many_As_Asked_For()
+        {
+            // arrange
+            var options = new WhateverOptions();
+
+            var builder = new OptionsBuilder<WhateverOptions>()
+                .WithSingleSwitch('s', (whateverOptions, s) => whateverOptions.Source = s)
+                .WithMultipleSwitch('t', (whateverOptions, strings) => whateverOptions.Things = strings, count: 3)
+                .WithMultipleSwitch('o',
+                    (whateverOptions, strings) =>
+                        whateverOptions.OtherThings = strings.Select(x => Convert.ToInt32(x)).ToArray())
+                .WithPositional(-1, (whateverOptions, strings) => whateverOptions.Target = string.Join(", ", strings));
+
+            // act
+            builder.Parse(options, "-t a b c d e f -s g h -o 1 2 3".Split(' '));
+            
+            // assert
+            options.Things.Should().BeEquivalentTo("a b c".Split(' '));
+            options.Source.Should().Be("g");
+            options.OtherThings.Should().BeEquivalentTo(new[] {1, 2, 3});
+            options.Target.Should().Be("d, e, f");
+        }
+    }
+}
---
+++ ArgParser/ArgParser.Core/OptionsBuilder.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace ArgParser.Core
+{
+    public interface IParser<TOptions> where TOptions : IOptions
+    {
+        TOptions Parse(string[] args);
+        string GetHelp(string question);
+    }
+
+    public interface IOptions
+    {
+
+    }
+
+    public interface ICommandLineElement
+    {
+        bool Required { get; }
+        string HelpText { get; }
+    }
+
+    internal abstract class Switch<TOptions> : ICommandLineElement where TOptions : IOptions
+    {
+        public char Letter { get; set; }
+        public string Word { get; set; }
+        public string HelpText { get; set; }
+        public bool Required { get; set; }
+    }
+
+    internal abstract class ValueSwitch<TOptions> : Switch<TOptions> where TOptions : IOptions
+    {
+        public Regex Regex { get; set; }
+
+    }
+
+    internal class BooleanSwitch<TOptions> : Switch<TOptions> where TOptions : IOptions
+    {
+        public Action<TOptions> Transformer { get; set; }
+    }
+
+    internal class SingleSwitch<TOptions> : ValueSwitch<TOptions> where TOptions : IOptions
+    {
+        public Action<TOptions, string> Transformer { get; set; }
+    }
+
+    internal class MultipleSwitch<TOptions> : ValueSwitch<TOptions> where TOptions : IOptions
+    {
+        public int Count { get; set; } = -1;
+        public Action<TOptions, string[]> Transformer { get; set; }
+    }
+
+    internal class PositionalValues<TOptions> : ICommandLineElement where TOptions : IOptions
+    {
+        public int Count { get; set; }
+        public Regex Regex { get; set; }
+        public Action<TOptions, string[]> Transformer { get; set; }
+
+        /// <inheritdoc />
+        public bool Required { get; set; }
+
+        /// <inheritdoc />
+        public string HelpText { get; set; }
+    }
+
+    public class MissingValueException : ArgumentException
+    {
+        public MissingValueException(string message) : base(message)
+        {
+        }
+    }
+
+    public class UnexpectedArgumentException : ArgumentException
+    {
+        /// <inheritdoc />
+        public UnexpectedArgumentException(string message) : base(message)
+        {
+        }
+    }
+
+    public class OptionsBuilder<TOptions> where TOptions : IOptions
+    {
+        internal IList<object> Order { get; set; } = new List<object>();
+        internal Dictionary<string, Switch<TOptions>> Switches = new Dictionary<string, Switch<TOptions>>();
+        internal IList<PositionalValues<TOptions>> Positionals = new List<PositionalValues<TOptions>>();
+        public OptionsBuilder<TOptions> WithBoolean(char letter, Action<TOptions> transformer)
+        {
+            var newGuy = new BooleanSwitch<TOptions>()
+            {
+                Letter = letter,
+                Transformer = transformer
+            };
+            Switches.Add($"-{letter}", newGuy);
+            Order.Add(newGuy);
+            return this;
+        }
+
+        public void Parse(TOptions instance, string[] args)
+        {
+            if (args == null || args.Length == 0)
+                return;
+            var positionalCounts = new Dictionary<PositionalValues<TOptions>, int>();
+            for (int i = 0; i < args.Length; i++)
+            {
+                var arg = args[i];
+
+                if (Switches.ContainsKey(arg))
+                {
+                    var s = Switches[arg];
+                    if (s is BooleanSwitch<TOptions> boolSwitch)
+                    {
+                        boolSwitch.Transformer(instance);
+                        continue;
+                    }
+
+                    if (s is SingleSwitch<TOptions> singleSwitch)
+                    {
+                        if(i + 1 >= args.Length)
+                            throw new MissingValueException($"Switch {arg} requires a value, but none was found.");
+                        var nextArg = args[i + 1];
+                        if(Switches.ContainsKey(nextArg))
+                            throw new MissingValueException($"Switch {arg} requires a value, but found another switch: {nextArg}");
+                        singleSwitch.Transformer(instance, nextArg);
+                    }
+
+                    if (s is MultipleSwitch<TOptions> multipleSwitch)
+                    {
+                        if (multipleSwitch.Count < 0)
+                        {
+                            var rest = args.Skip(i + 1).TakeWhile(a => !Switches.ContainsKey(a)).ToArray();
+                            multipleSwitch.Transformer(instance, rest);
+                            i += rest.Length;
+                            continue;
+                        }
+                        else
+                        {
+                            int j;
+                            List<string> multipleStrings = new List<string>();
+                            for (j = 0; i + j + 1 < args.Length && j < multipleSwitch.Count; j++)
+                            {
+                                var cur = args[i + j + 1];
+                                if (Switches.ContainsKey(cur))
+                                    throw new MissingValueException($"Switch {arg} requires a value, but found another switch: {cur}");
+                                multipleStrings.Add(cur);
+                            }
+                            if(multipleStrings.Count != multipleSwitch.Count)
+                                throw new MissingValueException($"Switch {arg} requires {multipleSwitch.Count} values, but only found {multipleStrings.Count}");
+                            multipleSwitch.Transformer(instance, multipleStrings.ToArray());
+                            i += multipleStrings.Count;
+                        }
+                    }
+                }
+                else
+                {
+                    var positional = Order.OfType<PositionalValues<TOptions>>().FirstOrDefault(x => !positionalCounts.ContainsKey(x));
+                    if(positional == null)
+                        continue;
+                    string[] newValues;
+                    if (positional.Count < 0)
+                    {
+                        newValues = args.Skip(i).TakeWhile(x => !Switches.ContainsKey(x)).ToArray();
+                    }
+                    else
+                    {
+                        newValues = args.Skip(i).TakeWhile(x => !Switches.ContainsKey(x)).Take(positional.Count).ToArray();
+                        if (newValues.Length != positional.Count)
+                            throw new MissingValueException($"Positional argument requires {positional.Count} values, but found {newValues.Length}");
+                    }
+                    positional.Transformer(instance, newValues);
+                    positionalCounts.Add(positional, newValues.Length);
+                    i += newValues.Length - 1;
+                }
+            }
+        }
+
+        public OptionsBuilder<TOptions> WithSingleSwitch(char letter, Action<TOptions, string> transformer)
+        {
+            var newGuy = new SingleSwitch<TOptions>()
+            {
+                Letter = letter,
+                Transformer = transformer
+            };
+            Switches.Add($"-{letter}", newGuy);
+            Order.Add(newGuy);
+            return this;
+        }
+
+        public OptionsBuilder<TOptions> WithMultipleSwitch(char letter, Action<TOptions, string[]> transformer, int count = -1)
+        {
+            var newGuy = new MultipleSwitch<TOptions>()
+            {
+                Letter = letter,
+                Transformer = transformer,
+                Count = count
+            };
+            Switches.Add($"-{letter}", newGuy);
+            Order.Add(newGuy);
+            return this;
+        }
+
+        public OptionsBuilder<TOptions> WithPositional(int count, Action<TOptions, string[]> transformer)
+        {
+            var newGuy = new PositionalValues<TOptions>()
+            {
+                Count = count,
+                Transformer = transformer
+            };
+            Positionals.Add(newGuy);
+            Order.Add(newGuy);
+            return this;
+        }
+    }
+}
