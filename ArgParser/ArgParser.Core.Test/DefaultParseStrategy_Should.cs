e378be3c0165d3553ab58912ee0d1beca56604d1
Ty Smith <dts5004@gmail.com>
2018-10-21 14:50:18 -0700

    Remade parser strategy concept

---
+++ ArgParser/ArgParser.Core.Test/DefaultParseStrategy_Should.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace ArgParser.Core.Test
+{
+    public class DefaultParseStrategy_Should
+    {
+        private class BaseOptions
+        {
+            public bool HelpRequested { get; set; }
+        }
+
+        [Fact]
+        public void Parse_A_Single_Type()
+        {
+            // arrange
+            var parser = new DefaultParser<BaseOptions>();
+            parser.AddParameter(new Parameter<BaseOptions>()
+            {
+                CanHandle = (instance, info) => info.Current.Raw == "-h" || info.Current.Raw == "--help",
+                Handle = (instance, info) =>
+                {
+                    instance.HelpRequested = true;
+                    return info.Consume(1);
+                }
+            });
+            var strat = new DefaultParseStrategy(new Func<object>[]{ () => new BaseOptions() });
+
+            // act
+            var result = strat.Parse(new[] {parser}, "--help".Split(' '));
+
+            // assert
+            bool isParsed = false;
+            result.When<BaseOptions>(options =>
+            {
+                options.HelpRequested.Should().BeTrue();
+                isParsed = true;
+            });
+            isParsed.Should().BeTrue();
+        }
+    }
+}
---
+++ ArgParser/ArgParser.Core/IParseStrategy.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArgParser.Core
+{
+    public interface IParseStrategy
+    {
+        IParseResult Parse(IEnumerable<IParser> parsers, string[] args);
+    }
+
+    public class DefaultParseStrategy : IParseStrategy
+    {
+        public IList<Func<object>> FactoryFunctions { get; set; } = new List<Func<object>>();
+        public IIterationInfoFactory IterationInfoFactory { get; set; } = new DefaultIterationInfoFactory();
+
+        /// <inheritdoc />
+        public DefaultParseStrategy(IEnumerable<Func<object>> factoryFuncs = null)
+        {
+            if (factoryFuncs == null) return;
+            foreach (var factoryFunc in factoryFuncs)
+            {
+                FactoryFunctions.Add(factoryFunc);
+            }
+        }
+
+        /// <inheritdoc />
+        public IParseResult Parse(IEnumerable<IParser> parsers, string[] args)
+        {
+            var info = IterationInfoFactory.Create(args);
+            var results = new List<object>();
+            foreach (var parser in parsers)
+            {
+                foreach (var factoryFunction in FactoryFunctions)
+                {
+                    var instance = factoryFunction();
+                    bool hasFailed = false;
+                    int last = 0;
+                    while (!hasFailed && !info.IsComplete && parser.CanConsume(instance, info))
+                    {
+                        info = parser.Consume(instance, info);
+                        if (info.Index <= last)
+                        {
+                            hasFailed = true;
+                        }
+                    }
+
+                    if (!hasFailed)
+                    {
+                        results.Add(instance);
+                    }
+                }
+            }
+            return new DefaultParseResult(results);
+        }
+    }
+}
