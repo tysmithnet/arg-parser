c10e3f808277ca0a6f4311ad272f64ab4e80afe0
Ty Smith <dts5004@gmail.com>
2018-10-21 13:21:56 -0700

    Added default parse strategy Does not work

--- ArgParser/ArgParser.Core.Test/DefaultIterationInfo_Should.cs
+++ ArgParser/ArgParser.Core.Test/DefaultIterationInfo_Should.cs
@@ -26,5 +26,33 @@ namespace ArgParser.Core.Test
             info.Index = 2;
             info.Current.Should().Be(new Token("c"));
         }
+
+        [Fact]
+        public void Consume_The_Correct_Amount_Of_Tokens()
+        {
+            var info = new DefaultIterationInfo();
+            info = (DefaultIterationInfo)info.SetTokens(new List<IToken>
+            {
+                new Token("a"),
+                new Token("b"),
+                new Token("c"),
+                new Token("d"),
+                new Token("e"),
+                new Token("f"),
+            });
+
+            // act
+            // assert
+            info.Index.Should().Be(0);
+            info.Consume(0).Index.Should().Be(0);
+            info.Consume(1).Index.Should().Be(1);
+            info.Consume(1).Current.Raw.Should().Be("b");
+            info.Consume(2).Index.Should().Be(2);
+            info.Consume(2).Current.Raw.Should().Be("c");
+            info.Consume(3).Consume(1).Index.Should().Be(4);
+            info.Consume(3).Consume(1).Current.Raw.Should().Be("e");
+            info.Consume(3).Consume(2).Index.Should().Be(5);
+            info.Consume(3).Consume(2).Current.Raw.Should().Be("f");
+        }
     }
 }
--- ArgParser/ArgParser.Core/IParseStrategy.cs
+++ ArgParser/ArgParser.Core/IParseStrategy.cs
@@ -3,26 +3,8 @@ using System.Text;
 
 namespace ArgParser.Core
 {
-    public interface IParseStrategy<in T>
+    public interface IParseStrategy<T>
     {
-        void AddParser<TSub>(IParser<TSub> parser) where TSub : T;
-        IParseResult Parse(string[] args);
-    }
-
-    public class DefaultParseStrategy<T> : IParseStrategy<T>
-    {
-        public IList<T> Parsers { get; set; }
-
-        /// <inheritdoc />
-        public void AddParser<TSub>(IParser<TSub> parser) where TSub : T
-        {
-
-        }
-
-        /// <inheritdoc />
-        public IParseResult Parse(string[] args)
-        {
-            throw new System.NotImplementedException();
-        }
+        IParseResult Parse<TSub>(IEnumerable<IParser<T>> parsers, string[] args) where TSub : T;
     }
 }
