d4fc4aa3167df7e89ab3ae81afc6ebb43c3ccd0c
Ty Smith <dts5004@gmail.com>
2018-10-21 21:24:13 -0700

    Added skeletons for help building

---
+++ ArgParser/ArgParser.Core/Help/IHelpNode.cs
@@ -0,0 +1,7 @@
+﻿namespace ArgParser.Core.Help
+{
+    public interface IHelpNode
+    {
+        void Accept(IHelpNodeVisitor visitor);
+    }
+}
---
+++ ArgParser/ArgParser.Core/Help/TableNode.cs
@@ -0,0 +1,11 @@
+﻿namespace ArgParser.Core.Help
+{
+    public class TableNode : IHelpNode
+    {
+        /// <inheritdoc />
+        public virtual void Accept(IHelpNodeVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+}
