fbbdd5676313ef1d28050f237bbd77c7cb79dc7d
Ty Smith <dts5004@gmail.com>
2018-10-21 21:53:31 -0700

    Added start of tests for default help builder

---
+++ ArgParser/ArgParser.Core.Test/DefaultHelpBuilder_Should.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ArgParser.Core.Help;
+using Xunit;
+
+namespace ArgParser.Core.Test
+{
+    public class DefaultHelpBuilder_Should
+    {
+        [Fact]
+        public void Return_The_Correct_Value_For_Null_Object()
+        {
+            // arrange
+            var builder = new DefaultHelpBuilder();
+            builder.AddGenericHelp()
+
+            // act
+
+            // assert
+        }
+    }
+}
---
+++ ArgParser/ArgParser.Core/Help/GenericHelp.cs
@@ -0,0 +1,20 @@
+﻿namespace ArgParser.Core.Help
+{
+    public class GenericHelp : IGenericHelp
+    {
+        /// <inheritdoc />
+        public string Version { get; set; }
+
+        /// <inheritdoc />
+        public string Name { get; set; }
+
+        /// <inheritdoc />
+        public string ShortDescription { get; set; }
+
+        /// <inheritdoc />
+        public string Description { get; set; }
+
+        /// <inheritdoc />
+        public string Author { get; set; }
+    }
+}
