06d6bf8769124edd86b7b14258801edf75eba817
Ty Smith <dts5004@gmail.com>
2018-10-22 02:51:13 -0700

    Split multi type files

--- ArgParser/ArgParser.Core/Validation/IValidationResult.cs
+++ ArgParser/ArgParser.Core/Validation/IValidationResult.cs
@@ -9,26 +9,8 @@ namespace ArgParser.Core.Validation
         object Instance { get; }
     }
 
-    public class ValidationResult : IValidationResult
-    {
-        /// <inheritdoc />
-        public bool IsSuccess { get; set; }
-
-        /// <inheritdoc />
-        public IList<ValidationError> Errors { get; set; }
-
-        /// <inheritdoc />
-        public object Instance { get; set; }
-    }
-
     public interface IValidationResult<out T> : IValidationResult
     {
         new T Instance { get; }
-    }
-
-    public class ValidationResult<T> : ValidationResult, IValidationResult<T>
-    {
-        /// <inheritdoc />
-        public new T Instance { get; set; }
     }
 }
---
+++ ArgParser/ArgParser.Core/Validation/ValidationResult.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace ArgParser.Core.Validation
+{
+    public class ValidationResult<T> : ValidationResult, IValidationResult<T>
+    {
+        /// <inheritdoc />
+        public new T Instance { get; set; }
+    }
+
+    public class ValidationResult : IValidationResult
+    {
+        /// <inheritdoc />
+        public bool IsSuccess { get; set; }
+
+        /// <inheritdoc />
+        public IList<ValidationError> Errors { get; set; }
+
+        /// <inheritdoc />
+        public object Instance { get; set; }
+    }
+}
