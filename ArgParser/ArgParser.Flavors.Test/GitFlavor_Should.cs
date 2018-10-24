9c2b6388ead9013b0dea939cea46e5db4e8713e8
Ty Smith <dts5004@gmail.com>
2018-10-24 00:06:12 -0700

    Added ArgParser.Flavors.Test project

---
+++ ArgParser/ArgParser.Flavors.Test/GitFlavor_Should.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ArgParser.Flavors.Test
+{
+    public class GitFlavor_Should
+    {
+        [Fact]
+        public void Identify_Help_Requested()
+        {
+            // arrange
+
+            // act
+
+            // assert
+        }
+    }
+}
