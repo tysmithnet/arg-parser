81e5671c2b99868d0df5cb08cdb6925a395ca134
Ty Smith <dts5004@gmail.com>
2018-10-23 20:13:47 -0700

    Started adding generic & non generic counter parts to core types

--- ArgParser/ArgParser.Core/DefaultParseStrategy.cs
+++ ArgParser/ArgParser.Core/DefaultParseStrategy.cs
@@ -1,9 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using ArgParser.Core.Validation;
 
 namespace ArgParser.Core
 {
+    public class DefaultParseStrategy<T> : DefaultParseStrategy, IParseStrategy<T>
+    {
+        public DefaultParseStrategy(IEnumerable<Func<object>> factoryFuncs = null)
+        {
+            foreach (var factoryFunc in factoryFuncs ?? new Func<object>[0]) FactoryFunctions.Add(factoryFunc);
+        }
+
+        /// <inheritdoc />
+        public IParseResult Parse(IEnumerable<IParser> parsers, string[] args)
+        {
+            var results = new List<object>();
+            foreach (var parser in parsers)
+            foreach (var factoryFunction in FactoryFunctions)
+            {
+                var info = IterationInfoFactory.Create(args);
+                var instance = factoryFunction();
+                var hasFailed = false;
+                var last = 0;
+                while (!hasFailed && !info.IsComplete && parser.CanConsume(instance, info))
+                {
+                    info = parser.Consume(instance, info);
+                    if (info.Index <= last) hasFailed = true;
+                }
+
+                var validationResults =
+                    Validators.Where(v => v.CanValidate(instance)).Select(v => v.Validate(instance));
+                var passedValidation = validationResults.All(r => r.IsSuccess);
+                if (!hasFailed && info.IsComplete && passedValidation) results.Add(instance);
+            }
+
+            return new DefaultParseResult(results);
+        }
+
+        public new IList<Func<T>> FactoryFunctions { get; set; } = new List<Func<T>>();
+
+        /// <inheritdoc />
+        public IParseResult Parse(IEnumerable<IParser<T>> parsers, string[] args)
+        {
+            
+        }
+    }
+
     public class DefaultParseStrategy : IParseStrategy
     {
         /// <inheritdoc />
