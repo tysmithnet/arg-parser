ae4b8a10b0ff26bacd39794e5d9d6e252fa1289a
Ty Smith <dts5004@gmail.com>
2018-10-28 23:10:36 -0700

    Split classes into their own files

---
+++ ArgParser/ArgParser.Flavors/GitParseStrategy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using ArgParser.Core;
+
+namespace ArgParser.Flavors
+{
+    public class GitParseStrategy : DefaultParseStrategy
+    {
+        public GitParseStrategy(List<Func<object>> factoryMethods) : base(factoryMethods)
+        {
+            
+        }
+
+        /// <inheritdoc />
+        protected override IParseResult CreateParseResult(List<object> results)
+        {
+            return new GitParseResult(results);
+        }
+    }
+}
---
+++ ArgParser/ArgParser.Flavors/ValuesSwitch.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using ArgParser.Core;
+
+namespace ArgParser.Flavors
+{
+    public class ValuesSwitch : Switch
+    {
+        /// <inheritdoc />
+        public override IIterationInfo Consume(object instance, IIterationInfo info)
+        {
+            var tokens = info.Rest.Select(x => TokenExtensions.ToGitToken(x)).TakeWhile(t => !t.IsAnyMatch).Select(t => t.Raw)
+                .ToArray();
+            // todo: check count
+            ConsumeCallback(instance, tokens);
+            return info.Consume(1 + tokens.Length);
+        }
+
+        public Action<object, string[]> ConsumeCallback { get; set; }
+    }
+}
