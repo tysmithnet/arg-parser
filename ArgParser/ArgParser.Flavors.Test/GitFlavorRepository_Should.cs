7f25074ee7e80a922b661088f3faf30967a9b42b
Ty Smith <dts5004@gmail.com>
2018-11-01 13:43:38 -0700

    Added additional tests for flavor repository

--- ArgParser/ArgParser.Flavors.Test/GitFlavorRepository_Should.cs
+++ ArgParser/ArgParser.Flavors.Test/GitFlavorRepository_Should.cs
@@ -25,5 +25,45 @@ namespace ArgParser.Flavors.Test
             mightThrow0.Should().NotThrow();
             mightThrow1.Should().Throw<ArgumentException>();
         }
+
+        [Fact]
+        public void Throw_If_Cant_Find_Parent_Or_Child_When_Creating_Relationships()
+        {
+            // arrange
+            var repo = new GitFlavorRepository();
+            repo.Create("parent");
+            //repo.Create("child");
+            Action mightThrow0 = () => repo.EstablishParentChildRelationship("parent", "child");
+            Action mightThrow1 = () => repo.EstablishParentChildRelationship("grandparent", "parent");
+
+            // act
+            // assert
+            mightThrow0.Should().Throw<KeyNotFoundException>();
+            mightThrow1.Should().Throw<KeyNotFoundException>();
+        }
+
+        [Fact]
+        public void Throw_If_Asking_For_A_Flavor_That_Doesnt_Exist()
+        {
+            // arrange
+            var repo = new GitFlavorRepository();
+            Action mightThrow = () => repo.Get("doesntexist");
+            
+            // act
+            // assert
+            mightThrow.Should().Throw<KeyNotFoundException>();
+        }
+
+        [Fact]
+        public void Throw_If_Asking_For_A_Parent_But_The_Child_Doesnt_Exist()
+        {
+            // arrange
+            var repo = new GitFlavorRepository();
+            Action mightThrow = () => repo.GetParent("doesntexist", false);
+
+            // act
+            // assert
+            mightThrow.Should().Throw<KeyNotFoundException>();
+        }
     }
 }
