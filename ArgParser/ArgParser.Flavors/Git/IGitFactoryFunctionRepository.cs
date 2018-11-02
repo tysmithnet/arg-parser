d00258fd840c67e18e0be0f288f49b6ae4c89b09
Ty Smith <dts5004@gmail.com>
2018-11-02 03:51:50 -0700

    Ran code cleanup

--- ArgParser/ArgParser.Flavors/Git/IGitFactoryFunctionRepository.cs
+++ ArgParser/ArgParser.Flavors/Git/IGitFactoryFunctionRepository.cs
@@ -6,7 +6,7 @@ namespace ArgParser.Flavors.Git
     public interface IGitFactoryFunctionRepository
     {
         void AddFactoryFunction(string parserName, Func<object> facFunc);
-        IEnumerable<Func<object>> GetFactoryFunctions(string parserName);
         bool Contains(string name);
+        IEnumerable<Func<object>> GetFactoryFunctions(string parserName);
     }
 }
