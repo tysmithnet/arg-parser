4c03c45ef702a6a551fde1e1349286b6515babdf
Ty Smith <dts5004@gmail.com>
2018-11-03 02:23:20 -0700

    Added a concept of theme

--- ArgParser/ArgParser.ColoredConsole/ColorfulHelpVisitor.cs
+++ ArgParser/ArgParser.ColoredConsole/ColorfulHelpVisitor.cs
@@ -1,19 +1,29 @@
 ﻿using System;
 using System.Linq;
 using Alba.CsConsoleFormat;
+using Alba.CsConsoleFormat.ColorfulConsole;
 using ArgParser.Core.Help;
 using ArgParser.Core.Help.Dom;
 
 namespace ArgParser.ColoredConsole
 {
+    public class Theme
+    {
+        public ConsoleColor TextColor { get; set; } = ConsoleColor.White;
+        public ConsoleColor CodeColor { get; set; } = ConsoleColor.Green;
+        public ConsoleColor HeadingColor { get; set; } = ConsoleColor.Cyan;
+    }
+
     public class ColorfulHelpVisitor : IHelpNodeVisitor<Element>
     {
+        public Theme Theme { get; set; } = new Theme();
+
         /// <inheritdoc />
         public Element Visit(TextNode node)
         {
             return new Span(node.Text)
             {
-                Color = ConsoleColor.Green
+                Color = Theme.TextColor
             };
         }
 
@@ -22,21 +32,22 @@ namespace ArgParser.ColoredConsole
         {
             return new Span(node.Text)
             {
-                Color = ConsoleColor.Yellow
+                Color = Theme.CodeColor
             };
         }
 
         /// <inheritdoc />
         public Element Visit(HeadingNode node)
         {
-            return new Span(node.Text)
+            return new FigletDiv()
             {
-                Color = ConsoleColor.Magenta
+                Text = node.Text,
+                Color = Theme.HeadingColor
             };
         }
 
         /// <inheritdoc />
-        public new Element Visit(RootNode node)
+        public Element Visit(RootNode node)
         {
             var children = node.Children.Select(x => x.Accept(this));
             return new Document(children);
