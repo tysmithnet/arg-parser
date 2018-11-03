a07aa9caab890377fe68f40fc5505f16730df58e
Ty Smith <dts5004@gmail.com>
2018-11-03 00:30:09 -0700

    Added test project for colored help writer

---
+++ ArgParser/ArgParser.ColoredHelpWriter.Test/ColoredHelpWriter_Should.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArgParser.ColoredHelpWriter.Test
+{
+    public class ColoredHelpWriter_Should
+    {
+    }
+}
