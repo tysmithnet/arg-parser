d4d0901d900d7a3f62c21526409220de58168582
Ty Smith <dts5004@gmail.com>
2018-11-04 15:15:45 -0800

    Continued readding solid concepts from before

---
+++ ArgParser/ArgParser.Core/ConsumptionResult.cs
@@ -0,0 +1,8 @@
+﻿namespace ArgParser.Core
+{
+    public class ConsumptionResult
+    {
+        public int NumConsumed { get; protected internal set; }
+        public IterationInfo Info { get; protected internal set; }
+    }
+}
--- ArgParser/ArgParser.Styles/Default/ParseStrategy.cs
+++ ArgParser/ArgParser.Styles/Default/ParseStrategy.cs
@@ -1,6 +1,43 @@
-﻿namespace ArgParser.Styles.Default
+﻿using System.Collections.Generic;
+using System.Linq;
+using ArgParser.Core;
+
+namespace ArgParser.Styles.Default
 {
-    public class ParseStrategy
+    public class ParseStrategy : IParseStrategy
     {
+        public IParseResult Parse(string[] args, IContext context)
+        {
+            // todo: arg check
+
+            var parsers = new List<Parser>();
+            
+            // find last sub command
+            int i = 0;
+            for (; i < args.Length; i++)
+            {
+                string left = i == 0 ? null : args[i - 1];
+                string right = args[i];
+                if (!context.HierarchyRepository.IsParent(left, right))
+                    break;
+                else
+                    parsers.Add(context.HierarchyRepository.Get(right));
+            }
+
+            var factoryFuncs = parsers.SelectMany(x => x.FactoryFunctions);
+
+            foreach (var fac in factoryFuncs)
+            {
+                var instance = fac();
+                var info = new IterationInfo(args, 0);
+                foreach (var parser in parsers)
+                {
+                    var potentialConsumptionResult = parser.CanConsume(instance, info);
+                    if (potentialConsumptionResult == info)
+                        continue;
+                    
+                }
+            }
+        }
     }
 }
