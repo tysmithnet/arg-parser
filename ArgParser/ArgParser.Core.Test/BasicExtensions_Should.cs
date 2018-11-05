9c5e91513ea485425b7e570dcbf7d4d3ca15f5cb
Ty Smith <dts5004@gmail.com>
2018-11-04 16:27:58 -0800

    Added ArgParser.Core.Test project

---
+++ ArgParser/ArgParser.Core.Test/BasicExtensions_Should.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace ArgParser.Core.Test
+{
+    public class BasicExtensions_Should
+    {
+        [Fact]
+        public void Identify_Null_Or_WhiteSpace_Strings()
+        {
+            // arrange
+            string sNull = null;
+
+            // act
+            // assert
+            sNull.IsNullOrWhiteSpace().Should().BeTrue();
+        }
+    }
+}
