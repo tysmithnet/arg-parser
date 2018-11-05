0fcccdd62d7933a995e2bdef01c44595b8defb30
Ty Smith <dts5004@gmail.com>
2018-11-04 22:13:33 -0800

    Continued adding tests

---
+++ ArgParser/ArgParser.Styles.Test/Default/ParserBuilder_Should.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ArgParser.Core;
+using ArgParser.Styles.Default;
+using FluentAssertions;
+using Xunit;
+
+namespace ArgParser.Styles.Test.Default
+{
+    public class ParserBuilder_Should
+    {
+        [Fact]
+        public void Allow_Boolean_Switches_To_Be_Added()
+        {
+            // arrange
+            var contextBuilder = new ContextBuilder();
+            var parser = new Parser("base");
+            var builder = new ParserBuilder(contextBuilder, parser);
+            int parseCount = 0;
+
+            // act
+            builder.WithBooleanSwitch('h', "help", o => parseCount++);
+
+            // assert
+            var result = (BooleanSwitch)parser.Parameters.Single();
+            result.Letter.Should().Be('h');
+            result.Word.Should().Be("help");
+        }
+
+        [Fact]
+        public void Return_The_Same_ContextBuilder_It_Was_Created_With()
+        {
+            // arrange
+            var contextBuilder = new ContextBuilder();
+            var parser = new Parser("base");
+            var builder = new ParserBuilder(contextBuilder, parser);
+
+            // act
+            // assert
+            builder.Build().Should().BeSameAs(contextBuilder);
+        }
+    }
+}
