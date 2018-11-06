83b3241d9235e5621b91ee7ec77cb895ad5c235a
Ty Smith <dts5004@gmail.com>
2018-11-05 18:39:27 -0800

    Added Positional class

---
+++ ArgParser/ArgParser.Styles/Default/Positional.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ArgParser.Core;
+
+namespace ArgParser.Styles.Default
+{
+    public class Positional : Parameter
+    {
+        public Positional(Action<object, string[]> consumeCallback) : base(consumeCallback)
+        {
+        }
+
+        public override ConsumptionResult CanConsume(object instance, IterationInfo info)
+        {
+            if(HasBeenConsumed)
+                return new ConsumptionResult(info, 0);
+            var values = info.FromNowOn().Take(MaxAllowed).ToArray();
+            return new ConsumptionResult(info, values.Length);
+        }
+    }
+
+    public class Positional<T> : Positional
+    {
+        private static Action<object, string[]> Convert(Action<T, string[]> action)
+        {
+            return (instance, s) =>
+            {
+                if (instance is T casted)
+                    action(casted, s);
+                else
+                    throw new ArgumentException(
+                        $"Expected to find object of type={typeof(T).FullName}, but found type={instance.GetType().FullName}");
+            };
+        }
+
+        public Positional(Action<T, string[]> consumeCallback) : base(Convert(consumeCallback))
+        {
+
+        }
+    }
+}
