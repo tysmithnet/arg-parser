ed555f01b2944f972b34669f493b0083ae04ebe1
Ty Smith <dts5004@gmail.com>
2018-11-08 23:57:19 -0800

    Added first bit of default help node writer

--- ArgParser/ArgParser.Core/Help/IHelpNodeVisitor.cs
+++ ArgParser/ArgParser.Core/Help/IHelpNodeVisitor.cs
@@ -6,5 +6,9 @@
         T Visit(RootNode node);
         T Visit(TextNode node);
         T Visit(HeadingNode node);
+        T Visit(BlockNode node);
+        T Visit(HorizontalLineNode node);
+        T Visit(GridNode node);
+        T Visit(CodeNode node);
     }
 }
---
+++ ArgParser/ArgParser.Styles/Default/HelpWriterVisitor.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ArgParser.Core.Help;
+
+namespace ArgParser.Styles.Default
+{
+    public class HelpWriter
+    {
+        public string CreateHelp(RootNode rootNode)
+        {
+            var visitor = new HelpWriterVisitor();
+            rootNode.Accept(visitor);
+            return visitor.Builder.ToString();
+        }
+    }
+
+    public class HelpWriterVisitor : IHelpNodeVisitor<object>
+    {
+        protected internal StringBuilder Builder { get; set; } = new StringBuilder();
+
+        /// <inheritdoc />
+        public object Visit(HelpNode node)
+        {
+            foreach (var nodeChild in node.Children)
+            {
+                nodeChild.Accept(this);
+            }
+
+            return null;
+        }
+
+        /// <inheritdoc />
+        public object Visit(RootNode node)
+        {
+            foreach (var nodeChild in node.Children)
+            {
+                nodeChild.Accept(this);
+            }
+
+            return null;
+        }
+
+        /// <inheritdoc />
+        public object Visit(TextNode node)
+        {
+            Builder.Append(node.Text);
+            return null;
+        }
+
+        /// <inheritdoc />
+        public object Visit(HeadingNode node)
+        {
+            Builder.AppendLine(node.Text);
+            return null;
+        }
+
+        /// <inheritdoc />
+        public object Visit(BlockNode node)
+        {
+            foreach (var nodeChild in node.Children)
+            {
+                nodeChild.Accept(this);
+            }
+
+            return null;
+        }
+
+        /// <inheritdoc />
+        public object Visit(HorizontalLineNode node)
+        {
+            Builder.AppendLine("------------------------------");
+            return null;
+        }
+
+        /// <inheritdoc />
+        public object Visit(GridNode node)
+        {
+            int n = node.Columns;
+            for (int i = 0; i < node.Children.Count; i++)
+            {
+                if (i % n == 0)
+                    Builder.AppendLine();
+                if (node.Children[i] is TextNode casted)
+                    Builder.Append($"\t{casted.Text}");
+            }
+            return null;
+        }
+
+        /// <inheritdoc />
+        public object Visit(CodeNode node)
+        {
+            Builder.Append(node.Text);
+            return null;
+        }
+    }
+}
