f6c2f9d9b6a99f2f41641e6141cb97126437efeb
Ty Smith <dts5004@gmail.com>
2018-11-11 22:01:59 -0800

    Added parameter chosen event for parser

---
+++ ArgParser/ArgParser.Core/ParameterChosenEventArgs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ArgParser.Core
+{
+    public class ParameterChosenEventArgs : EventArgs
+    {
+        public Parameter Parameter { get; protected internal set; }
+        public ConsumptionRequest Request { get; protected internal set; }
+
+        public ParameterChosenEventArgs(Parameter parameter, ConsumptionRequest request)
+        {
+            Parameter = parameter ?? throw new ArgumentNullException(nameof(parameter));
+            Request = request ?? throw new ArgumentNullException(nameof(request));
+        }
+    }
+}
