414f6e20bede77a2a855b79514260a2ba17bc803
Ty Smith <dts5004@gmail.com>
2018-11-12 23:15:55 -0800

    Added ContextDecorator

---
+++ ArgParser/ArgParser.Styles.Help.Alba/ContextDecorator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ArgParser.Core;
+
+namespace ArgParser.Styles.Help.Alba
+{
+    public class ContextDecorator
+    {
+        protected internal IContext Context { get; set; }
+
+        public IEnumerable<Switch> OwnSwitches { get; set; }
+        public IEnumerable<Switch> InheritedSwitches { get; set; }
+        public IEnumerable<Positional> Positionals { get; set; }
+        public IEnumerable<Positional> InheritedPositionals { get; set; }
+
+    }
+}
