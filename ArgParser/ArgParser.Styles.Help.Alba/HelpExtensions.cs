037d3985cf4cdd26e61b994778ea73ae10e430d2
Ty Smith <dts5004@gmail.com>
2018-11-12 20:02:53 -0800

    Refactored how help extensions is composed

--- ArgParser/ArgParser.Styles.Help.Alba/HelpExtensions.cs
+++ ArgParser/ArgParser.Styles.Help.Alba/HelpExtensions.cs
@@ -1,16 +1,42 @@
-﻿using ArgParser.Core;
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using ArgParser.Core;
+using Colorful;
 
 namespace ArgParser.Styles.Help.Alba
 {
     public static class HelpExtensions
     {
+        internal static Dictionary<ContextBuilder, HelpRenderer> Renderers { get; set; } = new Dictionary<ContextBuilder, HelpRenderer>();
+
         public static void RenderHelp(this IContext context, string parserId, int width = 80)
         {
-            var node = HelpNodeFactory.Create(parserId, context);
-            HelpWriter.RenderHelp(node, width);
+            var renderer = new HelpRenderer();
+            renderer.Render(context, parserId, width);
         }
 
-        internal static IHelpNodeFactory HelpNodeFactory { get; set; } = new HelpNodeFactory();
-        internal static IHelpWriter HelpWriter { get; set; } = new HelpWriter();
+        public static ContextBuilder AddMultiColoredCommands(this ContextBuilder builder, params Color[] colors)
+        {
+            var convertedColors = colors.PreventNull().Select(c => c.ToNearestConsoleColor()).ToArray();
+            return builder;
+        }
+    }
+
+    public class ColorfulSubCommandDecorator : HelpNodeFactory
+    {
+
+    }
+
+    public class HelpRenderer
+    {
+        public IHelpNodeFactory HelpNodeFactory { get; set; } = new HelpNodeFactory();
+        public IHelpWriter HelpWriter { get; set; } = new HelpWriter();
+
+        public void Render(IContext context, string parserId, int width)
+        {
+            var root = HelpNodeFactory.Create(parserId, context);
+            HelpWriter.RenderHelp(root, width);
+        }
     }
 }
