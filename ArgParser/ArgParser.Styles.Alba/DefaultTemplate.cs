84d6aabfc69d19d828a0def45db6b829b680e45a
Ty Smith <dts5004@gmail.com>
2018-11-14 01:00:30 -0800

    Split out multi definition files

---
+++ ArgParser/ArgParser.Styles.Alba/DefaultTemplate.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using Alba.CsConsoleFormat;
+using ArgParser.Core;
+
+namespace ArgParser.Styles.Alba
+{
+    public class DefaultTemplate : Template
+    {
+        public DefaultTemplate(IContext context) : base(context)
+        {
+        }
+
+        public override Document Create()
+        {
+            using (var fs = File.OpenRead("Views/DefaultTemplate.xaml"))
+            {
+                return ConsoleRenderer.ReadDocumentFromStream(fs, new Parser("util"), new XamlElementReaderSettings()
+                {
+                    ReferenceAssemblies =
+                    {
+                        typeof(DefaultTemplate).Assembly
+                    }
+                });
+            }
+        }
+    }
+}
---
+++ ArgParser/ArgParser.Styles.Alba/Template.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+using Alba.CsConsoleFormat;
+using ArgParser.Core;
+
+namespace ArgParser.Styles.Alba
+{
+    public abstract class Template
+    {
+        protected internal IContext Context { get; set; }
+
+        protected Template(IContext context)
+        {
+            Context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public abstract Document Create();
+    }
+}
