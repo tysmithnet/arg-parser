deeb7ced022209d0914db43de1368779df35791b
Ty Smith <dts5004@gmail.com>
2018-11-17 02:00:34 -0800

    Split multi definition files

---
+++ ArgParser/ArgParser.Styles.Alba/AlbaContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using ArgParser.Core;
+
+namespace ArgParser.Styles.Alba
+{
+    public class AlbaContext : IContext
+    {
+        protected internal IContext Context { get; set; }
+        public IHierarchyRepository HierarchyRepository => Context.HierarchyRepository;
+        public IParserRepository ParserRepository => Context.ParserRepository;
+        protected internal Dictionary<Parser, Theme> Theme { get; set; }
+        
+        public AlbaContext(IContext context)
+        {
+            Context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+    }
+}
---
+++ ArgParser/ArgParser.Styles.Alba/ParserHelpTemplateViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ArgParser.Core;
+
+namespace ArgParser.Styles.Alba
+{
+    public class ParserHelpTemplateViewModel
+    {
+        public ParserViewModel ParserVm => Chain.Last();
+        public IList<ParserViewModel> Chain { get; set; }
+        public IList<ParameterViewModel> ParameterVms { get; set; }
+
+        public string BannerColor =>
+            $"{ParserVm.Theme.DefaultTextColor} 0; {ParserVm.Theme.SecondaryTextColor}; {ParserVm.Theme.CodeColor} 3";
+
+        public string SubTitle
+        {
+            get
+            {
+                var sb = new StringBuilder(ParserVm.Parser.Id);
+                if (ParserVm.Parser.Help.Version.IsNotNullOrWhiteSpace())
+                    sb.Append($" - {ParserVm.Parser.Help.Version}");
+                if (ParserVm.Parser.Help.ShortDescription.IsNotNullOrWhiteSpace())
+                    sb.Append($" - {ParserVm.Parser.Help.ShortDescription}");
+                return sb.ToString();
+            }
+        }
+        
+    }
+}
