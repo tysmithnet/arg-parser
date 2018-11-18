a2eff693f3234088d643ff687555e271a4a3740c
Ty Smith <dts5004@gmail.com>
2018-11-18 00:17:09 -0800

    Minor clean up tasks

---
+++ ArgParser/ArgParser.Styles/HierarchyNode.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using ArgParser.Core;
+
+namespace ArgParser.Styles
+{
+    public class HierarchyNode
+    {
+        public HierarchyNode(string id)
+        {
+            Id = id.ThrowIfArgumentNull(nameof(id));
+        }
+
+        public void Add(HierarchyNode child)
+        {
+            Children.Add(child);
+            child.Parent = this;
+        }
+
+        public IList<HierarchyNode> Children { get; set; } = new List<HierarchyNode>();
+        public string Id { get; set; }
+        public HierarchyNode Parent { get; set; }
+    }
+}
