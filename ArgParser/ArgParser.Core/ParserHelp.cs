e08b24c415e2d19f234820e5dea624cbbc876a25
Ty Smith <dts5004@gmail.com>
2018-11-18 23:05:35 -0800

    Continued adding documentation

--- ArgParser/ArgParser.Core/ParserHelp.cs
+++ ArgParser/ArgParser.Core/ParserHelp.cs
@@ -1,9 +1,38 @@
-﻿namespace ArgParser.Core
+﻿// ***********************************************************************
+// Assembly         : ArgParser.Core
+// Author           : @tysmithnet
+// Created          : 11-18-2018
+//
+// Last Modified By : @tysmithnet
+// Last Modified On : 11-18-2018
+// ***********************************************************************
+// <copyright file="ParserHelp.cs" company="ArgParser.Core">
+//     Copyright (c) . All rights reserved.
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+namespace ArgParser.Core
 {
+    /// <summary>
+    /// Represents generic help for a parser
+    /// </summary>
+    /// <seealso cref="ArgParser.Core.FullHelp" />
     public class ParserHelp : FullHelp
     {
+        /// <summary>
+        /// Gets or sets the author.
+        /// </summary>
+        /// <value>The author.</value>
         public string Author { get; set; }
+        /// <summary>
+        /// Gets or sets the repository URL.
+        /// </summary>
+        /// <value>The repository URL.</value>
         public string RepositoryUrl { get; set; }
+        /// <summary>
+        /// Gets or sets the version.
+        /// </summary>
+        /// <value>The version.</value>
         public string Version { get; set; }
     }
 }
--- ArgParser/ArgParser.Core/UnexpectedArgException.cs
+++ ArgParser/ArgParser.Core/UnexpectedArgException.cs
@@ -1,12 +1,37 @@
-﻿namespace ArgParser.Core
+﻿// ***********************************************************************
+// Assembly         : ArgParser.Core
+// Author           : @tysmithnet
+// Created          : 11-04-2018
+//
+// Last Modified By : @tysmithnet
+// Last Modified On : 11-18-2018
+// ***********************************************************************
+// <copyright file="UnexpectedArgException.cs" company="ArgParser.Core">
+//     Copyright (c) . All rights reserved.
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+namespace ArgParser.Core
 {
+    /// <summary>
+    /// An exception that occurs when an argument that is not expected is encountered
+    /// </summary>
+    /// <seealso cref="ArgParser.Core.ParseException" />
     public class UnexpectedArgException : ParseException
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UnexpectedArgException"/> class.
+        /// </summary>
+        /// <param name="location">The location.</param>
         public UnexpectedArgException(IterationInfo location) : base($"Encountered unexpected argument={location.Current}")
         {
             Location = location.ThrowIfArgumentNull(nameof(location));
         }
 
+        /// <summary>
+        /// Gets or sets the location.
+        /// </summary>
+        /// <value>The location.</value>
         public IterationInfo Location { get; protected internal set; }
     }
 }
