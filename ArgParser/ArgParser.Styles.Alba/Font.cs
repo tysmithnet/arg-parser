550be97b24e880f3e215ba1db933af7f29e08072
Ty Smith <dts5004@gmail.com>
2018-11-21 10:28:54 -0800

    Started font additions

---
+++ ArgParser/ArgParser.Styles.Alba/Font.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ArgParser.Styles.Alba
+{
+    public abstract class Font
+    {
+        public abstract string Name { get; }
+        public abstract string FileName { get; }
+
+        public static readonly Font OneRow = new _1Row();
+
+        private class _1Row : Font
+        {
+            public override string Name { get; } = "1Row";
+            public override string FileName { get; } = "1row.flf";
+        }
+
+        private class _3D : Font
+        {
+            public override string Name { get; } = 
+            public override string FileName { get; }
+        }
+    }
+}
