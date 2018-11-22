fe4c166e2a7f6abab93f797feba0b81c3a91ac62
tysmithnet <dts5004@gmail.com>
2018-11-22 13:03:40 -0800

    Adding comments to BannerArtDiv

--- ArgParser/ArgParser.Styles.Alba/BannerArtDiv.cs
+++ ArgParser/ArgParser.Styles.Alba/BannerArtDiv.cs
@@ -1,19 +1,50 @@
-﻿using System;
+﻿// ***********************************************************************
+// Assembly         : ArgParser.Styles.Alba
+// Author           : dts50
+// Created          : 11-22-2018
+//
+// Last Modified By : dts50
+// Last Modified On : 11-22-2018
+// ***********************************************************************
+// <copyright file="BannerArtDiv.cs" company="tysmith.net">
+//     Copyright (c) . All rights reserved.
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+
 using System.Collections.Generic;
-using System.Text;
 using Alba.CsConsoleFormat;
 using ArgParser.Core;
 using Figgle;
 
 namespace ArgParser.Styles.Alba
 {
+    /// <summary>
+    ///     Element that renders an ASCII art banner
+    ///     Implements the <see cref="Alba.CsConsoleFormat.BlockElement" />
+    /// </summary>
+    /// <seealso cref="Alba.CsConsoleFormat.BlockElement" />
     public class BannerArtDiv : BlockElement
     {
+        /// <summary>
+        ///     Gets or sets the text.
+        /// </summary>
+        /// <value>The text.</value>
         public string Text { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the font.
+        /// </summary>
+        /// <value>The font.</value>
         public FiggleFont Font { get; set; }
+
+        /// <summary>
+        ///     Generates the visual elements.
+        /// </summary>
+        /// <returns>IEnumerable&lt;Element&gt;.</returns>
         public override IEnumerable<Element> GenerateVisualElements()
         {
-            string text = Font.Render(Text);
+            var text = Font.Render(Text);
             return new Span(text)
             {
                 Color = Color
