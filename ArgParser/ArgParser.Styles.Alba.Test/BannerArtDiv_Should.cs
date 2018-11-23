70e31c9af610257b892df7b04d90e817904eba0f
tysmithnet <dts5004@gmail.com>
2018-11-22 21:53:53 -0800

    Added BannerArt tests

---
+++ ArgParser/ArgParser.Styles.Alba.Test/BannerArtDiv_Should.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Alba.CsConsoleFormat;
+using Figgle;
+using FluentAssertions;
+using Xunit;
+
+namespace ArgParser.Styles.Alba.Test
+{
+    public class BannerArtDiv_Should
+    {
+        [Fact]
+        public void Render_Figlet_Font()
+        {
+            // arrange
+            var div = new BannerArtDiv()
+            {
+                Font = FiggleFonts.Poison,
+                Text = "hi"
+            };
+
+            // act
+            var elements = div.GenerateVisualElements();
+
+            // assert
+            elements.Cast<Span>().Single().Text.Should().Be(@"               
+@@@  @@@  @@@  
+@@@  @@@  @@@  
+@@!  @@@  @@!  
+!@!  @!@  !@!  
+@!@!@!@!  !!@  
+!!!@!!!!  !!!  
+!!:  !!!  !!:  
+:!:  !:!  :!:  
+::   :::   ::  
+ :   : :  :    
+               
+");
+        }
+    }
+}
