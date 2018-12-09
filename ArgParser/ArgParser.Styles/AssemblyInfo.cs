9642f2854c0a06251893e62f3f4b0e539a0f10eb
Ty Smith <dts5004@gmail.com>
2018-12-09 01:21:55 -0800

    Increased version to 1.0.0-rc2

--- ArgParser/ArgParser.Styles/AssemblyInfo.cs
+++ ArgParser/ArgParser.Styles/AssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("ArgParser.Styles.Test")]
 
-[assembly: AssemblyVersion("0.5.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")] // patched by appveyor
